df880cf1a201af6a75618a97d5f40bb81a65c7f7
ry129 <0111cs211146.riya@gmail.com>
2025-03-12 13:25:26 +0530

    EmployeePayrollUIAdded

---
+++ Employee-Payroll-Full_Stack/src/app/form-component/form-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-component',
+  imports: [],
+  templateUrl: './form-component.component.html',
+  styleUrl: './form-component.component.css'
+})
+export class FormComponentComponent {
+
+}
